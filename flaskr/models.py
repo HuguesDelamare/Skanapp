b827bae42327e85184128ac7f64ff9b8d2936c2f
HuguesDelamare <bluekikiller74@gmail.com>
2024-02-17 20:35:13 +0100

    Remove commented out code and unused models

--- flaskr/models.py
+++ flaskr/models.py
@@ -3,21 +3,6 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-
-"""
-class ShopCities(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    city_name = db.Column(db.String(255), nullable=False)
-
-
-class ShopsList(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    shop_name = db.Column(db.String(255), nullable=False)
-    shop_city_id = db.Column(db.Integer, db.ForeignKey('shop_cities.id'), nullable=False)
-    shop_city = db.relationship('ShopCities', backref=db.backref('shops_list', lazy=True))
-"""
-
-
 class Receipts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     shop_name = db.Column(db.String(255), nullable=False)
@@ -26,12 +11,10 @@ class Receipts(db.Model):
     receipt_amount = db.Column(db.Float, nullable=False)
     receipt_datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Float, nullable=False)
-
 
 class ReceiptProducts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
